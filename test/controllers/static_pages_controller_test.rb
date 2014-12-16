e07f57a690dcbeda2fc1d7955def09ae788628d6
RonLeapYearBoy <RonLeapYearBoy@yahoo.com>
2014-12-16 11:51:28 -0500

    Copied 3.4.4 and 3.5 Conclusion.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,12 +1,12 @@
 # --------------static_pages_controller_test.rb ------------------------------------ #
-# ---        09.000-Static-Pages-Controller-Test.txt                             --- #
+# ---        12.000-Static-Pages-Controller-Test.txt                             --- #
 # ---------------------------------------------------------------------------------- #
 # ---                                                                            --- #
-# 3.4.1 Testing titles (Red)                                                         #
-# https://www.railstutorial.org/book/static_pages#sec-testing_titles                 #
+# 3.6 Exercises                                                                      #
+# https://www.railstutorial.org/book/static_pages#sec-static_pages_exercises         #
 #                                                                                    #
-# Listing 3.22: The Static Pages controller test with title tests.                   #
-# https://www.railstutorial.org/book/static_pages#code-custom_home_page              #
+# Listing 3.38:  The Static Pages controller test with a base title.                 #
+# https://www.railstutorial.org/book/static_pages#code-base_title_test               #
 #                                                                                    #
 #  gedit test/controllers/static_pages_controller_test.rb                            #
 #                                                                                    #
@@ -18,30 +18,27 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@base_title}"
   end
-
-  test "should get contactt" do
-    get :contact
-    assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
-  end
-
 end
 # ---------------------------------------------------------------------------------- #
 
